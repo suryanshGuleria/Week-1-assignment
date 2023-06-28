c8a748b235861249f38f95620b86185cb948ae0d
suryanshGuleria <suryanshguleria05@gmail.com>
2023-06-28 22:48:00 +0530

    Assignment easy1 done.

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -7,8 +7,31 @@
   - `npm run test-anagram`
 */
 
+function stringToArray(str){
+     var arrayOfLetters = [];
+     for(let i = 0; i<str.length; i++){
+     arrayOfLetters[i] = str.charAt(i);
+    }
+    return arrayOfLetters.sort();
+}
+
+function compareArrays(arr1, arr2){
+   return arr1.toString() === arr2.toString();
+}
+
 function isAnagram(str1, str2) {
+     var arrayStr1 = stringToArray(str1);
+     var arrayStr2 = stringToArray(str2);
+     var answer = compareArrays(arrayStr1, arrayStr2);
+     if(answer === true)
+     console.log('Yes given string is an anagram');
+     else
+     console.log('No given string is not an anagram');
 
 }
+
+var name1 = 'suryansh';
+var name2 = 'suryansh';
+isAnagram(name1, name2); 
 
 module.exports = isAnagram;
